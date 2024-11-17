573c0528af9a13d31e46c5a2e9a24700d9e097ee
Christopher Begnaud <christopherbegnaud@gmail.com>
2024-11-17 16:17:17 -0700

    Daily codewars challenge.

---
+++ 2024-11-17/app.js
@@ -0,0 +1,13 @@
+const updateLight = (current) => { 
+    switch (current) {
+        case 'green':
+        return 'yellow';
+        break;
+        case 'yellow':
+        return 'red';
+        break;
+        case 'red':
+        return 'green';
+        break;
+    }
+  }
