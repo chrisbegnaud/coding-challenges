e083af6cd29666595b9a27a7d77f908fe6254a7e
Christopher Begnaud <christopherbegnaud@gmail.com>
2024-12-15 19:59:51 -0700

    Capitalize letters that occupy even indexes and odd indexes separately

---
+++ alternateCapitalization.js
@@ -0,0 +1,13 @@
+function capitalize(s){
+  let str1 = '', str2 = '';
+  for (let i = 0; i < s.length; i++) {
+    if (i === 0 || i % 2 === 0) {
+      str1 += s[i].toUpperCase();
+      str2 += s[i];
+    } else {
+      str1 += s[i];
+      str2 += s[i].toUpperCase();
+    }
+  }
+    return [str1, str2];
+};
